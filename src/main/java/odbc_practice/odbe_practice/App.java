cc6447d80e3e266f6ca29d829a33f2d93ded1bda
Xiao Song <song0137@algonquinlive.com>
2024-07-12 22:33:15 -0400

    initial

---
+++ src/main/java/odbc_practice/odbe_practice/App.java
@@ -0,0 +1,13 @@
+package odbc_practice.odbe_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
